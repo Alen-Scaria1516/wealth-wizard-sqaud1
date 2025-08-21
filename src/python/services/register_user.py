a73e3a21830101a5e10db1e9dc0ad7476bf7db75
Abhigna0701 <ls9204@srmist.edu.in>
2025-08-21 16:04:34 +0530

    Commit - Registration

--- src/python/services/register_user.py
+++ src/python/services/register_user.py
@@ -2,6 +2,8 @@ import oracledb
 from datetime import datetime
 import re
 import bcrypt
+from getpass import getpass
+from utils.send_email import send_registration_email
 
 #strong password
 def is_strong_password(password):
@@ -34,12 +36,23 @@ def register_user(connection):
 
     #weak password
     while True:
-        password = input("Enter your password: ")
+        print("\nEnter your password (input will be hidden):")
+        password = getpass("Password: ")
+
+        print("Re-enter your password (input will be hidden):")
+        confirm_password = getpass("Confirm Password: ")
+        print(type(password))
+
+        if password != confirm_password:
+            print("Passwords do not match. Try again.\n")
+            continue
+
         if is_strong_password(password):
             break
         else:
-            print("Weak password. Please try again following the above rules.\n")
+            print("Weak password. Please follow the rules.\n")
     
+    print(type(password))
     hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
     #user_id
     cursor.execute("SELECT 'U' || LPAD(user_seq.NEXTVAL, 3, '0') FROM dual")
@@ -48,19 +61,24 @@ def register_user(connection):
     cursor.execute("SELECT 'R' || LPAD(user_profile_seq.NEXTVAL, 3, '0') FROM dual")
     reg_id = cursor.fetchone()[0]
     try:
-        # Insert into users
+        # Insert into Users
         cursor.execute("""
             INSERT INTO Users (User_ID, Email_ID, Password, last_modified)
             VALUES (:1, :2, :3, :4)
         """, (user_id, email, hashed_password, datetime.now()))
+
         # Insert into user_details
         cursor.execute("""
             INSERT INTO user_details (reg_id, user_id, name, age)
             VALUES (:1, :2, :3, :4)
         """, (reg_id, user_id, name, age))
+
         connection.commit()
         print("Registration successful!")
         print(f"Your User ID: {user_id}, Registration ID: {reg_id}")
+
+        # send email
+        send_registration_email(email, user_id, reg_id)
 
     except oracledb.IntegrityError:
         print("Error: Email already exists.")
